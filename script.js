f603a7b6767d8d0e338e0242bb56698d68eb2be8
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-21 20:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const fib=(num)=>{
-   if (num<2){
-      return num
-   } else {
-      return fib(num-1)+fib(num-2)
-   }
-}
+function F(num) {  if(num== 0) {    return 0;
+ }  if(num == 1) {    return 1;
+ }  else {    return F(num-1) + F(num-2);
+ }}
